945c6e6e6d8758e94769ce8d3a2292237af48e88
Audun Stien <auduns@stacc.com>
2021-09-30 16:53:00 +0200

    Add expert mode

--- main.go
+++ main.go
@@ -24,23 +24,28 @@ type Word struct {
 var totbCmd = &cobra.Command{
 	Use: "totb",
 	Run: func(cmd *cobra.Command, args []string) {
-
 		word, err := GetWord()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Print(WordStr(word))
+
+		expert, err := cmd.Flags().GetBool("expert")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Print(WordStr(word, expert))
 
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			if scanner.Text() == word.Word {
-				fmt.Printf("\033[1A\033[K\033[1A\033[K\u2714 %s", WordStr(word))
+				fmt.Printf("\033[1A\033[K\033[1A\033[K\u2714 %s", WordStr(word, false))
 				word, err = GetWord()
 				if err != nil {
 					panic(err)
 				}
 
-				fmt.Print(WordStr(word))
+				fmt.Print(WordStr(word, expert))
 			} else {
 				fmt.Print("\033[1A\033[K")
 			}
@@ -66,8 +71,16 @@ func GetWord() (*Word, error) {
 	return &words[0], nil
 }
 
-func WordStr(w *Word) string {
-	return fmt.Sprintf("%s (%s) - %s\n", w.Word, w.Pronunciation, w.Definition)
+func WordStr(w *Word, expert bool) string {
+	if expert {
+		return fmt.Sprintf("%s - %s\n", w.Pronunciation, w.Definition)
+	} else {
+		return fmt.Sprintf("%s (%s) - %s\n", w.Word, w.Pronunciation, w.Definition)
+	}
+}
+
+func init() {
+	totbCmd.Flags().BoolP("expert", "e", false, "Expert Mode")
 }
 
 func main() {
