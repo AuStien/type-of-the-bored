46b62fa8dd556a09f16fb340bbc20fb0a9e36683
Audun Stien <audun97@gmail.com>
2023-10-30 14:05:02 +0000

    feat: add ability to specify port with PORT envvar

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/austien/type-of-the-bored/http"
 )
 
 func main() {
-	const addr = ":8080"
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+
 	slog.Info("Starting server", "addr", addr)
 	if err := http.ListenAndServe(addr); err != nil {
 		slog.Error("server failed", "addr", addr, "err", err)
